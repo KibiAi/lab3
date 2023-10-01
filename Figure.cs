941f3d49b3022223620feb3716e984a26d98b8ad
{{KibiAi}} <{{vetal2004v@gmail.com}}>
2023-10-01 20:04:33 +0300

    Добавьте файлы проекта.

---
+++ Figure.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab3
+{
+    class Figure
+    {
+        private Point[] points;
+
+        public Figure(Point p1, Point p2, Point p3)
+        {
+            points = new Point[3] { p1, p2, p3 };
+        }
+
+        public Figure(Point p1, Point p2, Point p3, Point p4)
+        {
+            points = new Point[4] { p1, p2, p3, p4 };
+        }
+
+        public Figure(Point p1, Point p2, Point p3, Point p4, Point p5)
+        {
+            points = new Point[5] { p1, p2, p3, p4, p5 };
+        }
+
+        public double GetSideLength(Point A, Point B)
+        {
+            double sideLength = Math.Sqrt(Math.Pow(B.X - A.X, 2) + Math.Pow(B.Y - A.Y, 2));
+            return sideLength;
+        }
+
+        public void CalculatePerimeter()
+        {
+            double perimeter = 0;
+            for (int i = 0; i < points.Length; i++)
+            {
+                int nextIndex = (i + 1) % points.Length;
+                perimeter += GetSideLength(points[i], points[nextIndex]);
+            }
+
+            Console.WriteLine($"Назва багатокутника: {points.Length} кут");
+            Console.WriteLine($"Периметр багатокутника: {perimeter}");
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using lab3;
+using System.Drawing;
+
+
+class Program
+{
+    static void Main ()
+    {
+        Console.WriteLine("Введіть довжину першої сторони прямокутника:");
+        double side1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Введіть довжину другої сторони прямокутника:");
+        double side2 = Convert.ToDouble(Console.ReadLine());
+
+        lab3.Rectangle rectangle = new lab3.Rectangle(side1, side2);
+
+        Console.WriteLine($"Площа прямокутника: {rectangle.Area}");
+        Console.WriteLine($"Периметр прямокутника: {rectangle.Perimeter}");
+
+        Console.ReadLine();
+
+        lab3.Point point1 = new lab3.Point(0, 0, "A");
+        lab3.Point point2 = new lab3.Point(0, 4, "B");
+        lab3.Point point3 = new lab3.Point(3, 0, "C");
+
+        Figure triangle = new Figure(point1, point2, point3);
+        triangle.CalculatePerimeter();
+
+        lab3.Point point4 = new lab3.Point(0, 0, "A");
+        lab3.Point point5 = new lab3.Point(0, 4, "B");
+        lab3.Point point6 = new lab3.Point(3, 4, "C");
+        lab3.Point point7 = new lab3.Point(3, 0, "D");
+
+        Figure quadrilateral = new Figure(point4, point5, point6, point7);
+        quadrilateral.CalculatePerimeter();
+
+        Console.ReadLine();
+    }
+}
+
